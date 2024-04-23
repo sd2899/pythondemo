cbb701d3492ad1d32f18901aa73228aef80d261f
sd2899 <34939012+sd2899@users.noreply.github.com>
2024-04-23 17:30:27 +0530

    Update test_app1.py

--- test_app1.py
+++ test_app1.py
@@ -1,11 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.binary_location = r"/snap/bin/firefox/"
+driver = webdriver.Firefox(options=options, executable_path="/snap/bin/firefox/")
+
 #from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 #from selenium import webdriver
 #firefox_binary = FirefoxBinary()
 #driver = webdriver.Firefox(firefox_binary=firefox_binary)
 
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
+#from selenium import webdriver
+#from selenium.webdriver.firefox.service import Service as FirefoxService
+#from webdriver_manager.firefox import GeckoDriverManager
 
 
 #driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
@@ -13,7 +20,7 @@ from webdriver_manager.firefox import GeckoDriverManager
 # Initialize the WebDriver 
 #driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
-driver = webdriver.Firefox("/snap/bin/firefox/")
+#driver = webdriver.Firefox("/snap/bin/firefox/")
 
 # Navigate to the Flask application's URL
 driver.get("http://172.29.233.42:5000")
