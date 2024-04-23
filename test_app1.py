44dc766607c531f9de548b3664da09b8195a3889
sd2899 <34939012+sd2899@users.noreply.github.com>
2024-04-23 16:54:19 +0530

    Update test_app1.py

--- test_app1.py
+++ test_app1.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
 # Initialize the WebDriver 
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+#driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
 #driver = webdriver.Firefox()
 
