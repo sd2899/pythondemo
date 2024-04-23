f513ba3d5a4fb6393f09c0c482fe5b4ca990046b
sd2899 <34939012+sd2899@users.noreply.github.com>
2024-04-23 17:14:44 +0530

    Update selenium_testcase.py

--- selenium_testcase.py
+++ selenium_testcase.py
@@ -2,12 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+#driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 # Initialize the WebDriver 
 #driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
-#driver = webdriver.Firefox()
+driver = webdriver.Firefox("/usr/bin/firefox")
 
 # Navigate to the Flask application's URL
 driver.get("http://172.29.233.42:5000")
